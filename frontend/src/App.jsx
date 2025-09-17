2a298f98e58ca59e1d5911273c34d54bb1a4b8f4
小川 哲平 <96569107+PK-2736@users.noreply.github.com>
2025-09-17 15:22:57 +0000

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DiscordCallback from './DiscordCallback';
-
-const DISCORD_CLIENT_ID = process.env.REACT_APP_DISCORD_CLIENT_ID;
-const REDIRECT_URI = process.env.REACT_APP_DISCORD_REDIRECT_URI;
-const OAUTH_URL = `https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20email`;
-
-function Home() {
-  return (
-    <div>
-      <h1>Hello, rectbot frontend!</h1>
-      <a href={OAUTH_URL}>
-        <button>Discordでログイン</button>
-      </a>
-    </div>
-  );
-}
+import Home from './Home';
 
 function App() {
   return (
