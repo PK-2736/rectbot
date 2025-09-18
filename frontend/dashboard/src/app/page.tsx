f20c9425037eeca060e7e8d930357546715da441
小川 哲平 <96569107+PK-2736@users.noreply.github.com>
2025-09-18 09:06:53 +0000

    test

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
-"use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 import AdminDashboard from "../components/AdminDashboard";
 
